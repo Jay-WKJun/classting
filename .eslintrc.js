ddc743b90ceaa2ec3695dc1bfee45a5a25d91169
KyungJun Woo <natcho9010@gmail.com>
2024-03-02 18:10:25 +0900

    chore: init project

---
+++ .eslintrc.js
@@ -0,0 +1,96 @@
+/** @type {import("eslint").Linter.Config} */
+module.exports = {
+  env: {
+    node: true,
+    browser: true,
+  },
+  parser: '@typescript-eslint/parser',
+
+  plugins: ['@typescript-eslint', 'import', 'prettier', 'unused-imports'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'airbnb-base', 'prettier', 'next/core-web-vitals'],
+  settings: {
+    'import/parser': {
+      '@typescript-eslint/parser': ['.ts', '.tsx'],
+    },
+    'import/resolver': {
+      typescript: {
+        alwaysTryTypes: true,
+      },
+      node: { extensions: ['.js', '.jsx', '.ts', '.tsx'] },
+    },
+  },
+
+  rules: {
+    'prettier/prettier': 'error',
+
+    'no-use-before-define': [
+      'error',
+      {
+        functions: false,
+      },
+    ],
+    'no-shadow': 'off',
+    'no-alert': 'off',
+    'no-param-reassign': 'off',
+    'no-underscore-dangle': 'off',
+    'no-undef': 'off',
+    'no-useless-escape': 'off',
+    'no-cond-assign': 'off',
+    'class-methods-use-this': 'off',
+    'prefer-destructuring': 'off',
+
+    'unused-imports/no-unused-imports': 'error',
+    'unused-imports/no-unused-vars': [
+      'warn',
+      { vars: 'all', varsIgnorePattern: '^_', args: 'after-used', argsIgnorePattern: '^_' },
+    ],
+
+    'import/no-extraneous-dependencies': 'off',
+    'import/no-absolute-path': 'off',
+    'import/extensions': 'off',
+    'import/export': 'off',
+    'import/prefer-default-export': 'off',
+    'import/order': [
+      'error',
+      {
+        'newlines-between': 'always',
+        groups: [['builtin', 'external'], 'internal', 'parent', 'sibling', 'index'],
+        pathGroups: [
+          {
+            pattern: '{react*,react*/**,next*,next*/**}',
+            group: 'external',
+            position: 'before',
+          },
+        ],
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
+        },
+      },
+    ],
+
+    '@typescript-eslint/no-empty-interface': 'off',
+
+    // React
+    'react/function-component-definition': [
+      'error',
+      { namedComponents: 'function-declaration', unnamedComponents: 'arrow-function' },
+    ],
+    'react/jsx-filename-extension': ['error', { extensions: ['.jsx', '.tsx'] }],
+    'react/jsx-props-no-spreading': 'off',
+    'react/prop-types': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react/require-default-props': 'off',
+    'react/no-unknown-property': ["error", { "ignore": ["css"] }],
+
+    'jsx-a11y/no-static-element-interactions': 'off',
+    'jsx-a11y/click-events-have-key-events': 'off',
+    'jsx-a11y/mouse-events-have-key-events': 'off',
+  },
+  ignorePatterns: [
+    // Ignore dotfiles
+    ".*.js",
+    "node_modules/",
+    "dist/",
+  ],
+}
