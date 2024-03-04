9948e163485ee2e48769dacdee1fd73897ba8595
KyungJun Woo <natcho9010@gmail.com>
2024-03-04 14:29:06 +0900

    test: add QuizSelections storybook test

---
+++ src/components/QuizSelections/QuizSelections.stories.tsx
@@ -0,0 +1,75 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import {useArgs} from '@storybook/preview-api';
+
+import { selectQuiz } from '@/models/QuizModel';
+
+import {mockQuizs} from '@/mock/quizsMock';
+import { QuizSelections } from './QuizSelections';
+
+const meta = {
+  title: 'Quiz/QuizSelections',
+  component: QuizSelections,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+} satisfies Meta<typeof QuizSelections>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+const quizMock = mockQuizs[0];
+const quizMock2 = mockQuizs[1];
+
+export const Primary: Story = {
+  args: {
+    quiz: quizMock,
+  },
+  render: function Render(args) {
+    const [{ quiz }, setArgs] = useArgs();
+
+    const handleSelectionClick = (_: React.MouseEvent, selectionIndex: number) => {
+      const {quiz} = args;
+      const selectedQuiz = selectQuiz(quiz, selectionIndex);
+      setArgs({ quiz: selectedQuiz });
+    };
+
+    return <QuizSelections {...args} quiz={quiz} onClick={handleSelectionClick} />;
+  }
+};
+
+export const Second: Story = {
+  args: {
+    quiz: quizMock2,
+  },
+  render: function Render(args) {
+    const [{ quiz }, setArgs] = useArgs();
+
+    const handleSelectionClick = (_: React.MouseEvent, selectionIndex: number) => {
+      const {quiz} = args;
+      const selectedQuiz = selectQuiz(quiz, selectionIndex);
+      setArgs({ quiz: selectedQuiz });
+    };
+
+    return <QuizSelections {...args} quiz={quiz} onClick={handleSelectionClick} />;
+  }
+};
+
+export const WithHold: Story = {
+  args: {
+    hold: true,
+    quiz: quizMock,
+  },
+  render: function Render(args) {
+    const [{ quiz }, setArgs] = useArgs();
+
+    const handleSelectionClick = (_: React.MouseEvent, selectionIndex: number) => {
+      const {quiz} = args;
+      const selectedQuiz = selectQuiz(quiz, selectionIndex);
+      setArgs({ quiz: selectedQuiz });
+    };
+
+    return <QuizSelections {...args} quiz={quiz} onClick={handleSelectionClick} />;
+  }
+};
+
---
+++ src/mock/quizsMock.ts
@@ -0,0 +1,125 @@
+import { QuizModel } from "@/models/QuizModel";
+
+// @ts-expect-error: This is quizmodel
+export const mockQuizs: QuizModel[] = [
+  {
+    "type": "multiple",
+    "difficulty": "easy",
+    "category": "Entertainment: Video Games",
+    "question": "Gordon Freeman is said to have burnt and destroyed what food in the break room microwave?",
+    "correctAnswer": "Casserole",
+    "incorrectAnswers": [
+      "Sub Sandwich",
+      "Chicken Soup",
+      "Pepperoni Pizza"
+    ],
+    "selections": [
+      {
+        "content": "Casserole",
+        "index": 0,
+        "isCorrect": true,
+        "isSelected": false
+      },
+      {
+        "content": "Pepperoni Pizza",
+        "index": 1,
+        "isCorrect": false,
+        "isSelected": false
+      },
+      {
+        "content": "Chicken Soup",
+        "index": 2,
+        "isCorrect": false,
+        "isSelected": false
+      },
+      {
+        "content": "Sub Sandwich",
+        "index": 3,
+        "isCorrect": false,
+        "isSelected": false
+      }
+    ],
+    "correctAnswerIndex": 0,
+    "selectedIndex": null
+  },
+  {
+    "type": "multiple",
+    "difficulty": "easy",
+    "category": "Entertainment: Music",
+    "question": "Which member of the Foo Fighters was previously the drummer for Nirvana?",
+    "correctAnswer": "Dave Grohl",
+    "incorrectAnswers": [
+      "Taylor Hawkins",
+      "Nate Mendel",
+      "Chris Shiflett"
+    ],
+    "selections": [
+      {
+        "content": "Nate Mendel",
+        "index": 0,
+        "isCorrect": false,
+        "isSelected": false
+      },
+      {
+        "content": "Chris Shiflett",
+        "index": 1,
+        "isCorrect": false,
+        "isSelected": false
+      },
+      {
+        "content": "Dave Grohl",
+        "index": 2,
+        "isCorrect": true,
+        "isSelected": false
+      },
+      {
+        "content": "Taylor Hawkins",
+        "index": 3,
+        "isCorrect": false,
+        "isSelected": false
+      }
+    ],
+    "correctAnswerIndex": 2,
+    "selectedIndex": null
+  },
+  {
+    "type": "multiple",
+    "difficulty": "easy",
+    "category": "Entertainment: Music",
+    "question": "Which Disney character sings the song \"A Dream is a Wish Your Heart Makes\"?",
+    "correctAnswer": "Cinderella",
+    "incorrectAnswers": [
+      "Belle",
+      "Snow White",
+      "Pocahontas"
+    ],
+    "selections": [
+      {
+        "content": "Cinderella",
+        "index": 0,
+        "isCorrect": true,
+        "isSelected": false
+      },
+      {
+        "content": "Snow White",
+        "index": 1,
+        "isCorrect": false,
+        "isSelected": false
+      },
+      {
+        "content": "Belle",
+        "index": 2,
+        "isCorrect": false,
+        "isSelected": false
+      },
+      {
+        "content": "Pocahontas",
+        "index": 3,
+        "isCorrect": false,
+        "isSelected": false
+      }
+    ],
+    "correctAnswerIndex": 0,
+    "selectedIndex": null
+  }
+] as QuizModel[];
